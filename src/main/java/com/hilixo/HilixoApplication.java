0e448cd52fb3949472fc9a4dd61467328ca7ebd2
root <root@vps1255520.vs.webtropia-customer.com>
2016-12-19 14:39:17 +0100

    initial commit

---
+++ src/main/java/com/hilixo/HilixoApplication.java
@@ -0,0 +1,12 @@
+package com.hilixo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilixoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HilixoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hilixo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.hilixo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+	@GetMapping
+	public String home() {
+		return "Hello";
+	}
+
+	
+}
